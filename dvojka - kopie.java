8e1de6b3e0a7a701e615d4fe8f32ace34b3333ad
frakyn <frantisek.kynych@tul.cz>
2018-04-20 18:34:27 +0200

    test

---
+++ dvojka - kopie.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sti_kymakun_2018;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * Třída pro reprezentaci commitu v tabulce
+ * @author beola
+ */
+public class TableCommit {
+
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty username;
+    private final SimpleStringProperty message;
+
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+    public String getDate() {
+        return date.get();
+    }
+
