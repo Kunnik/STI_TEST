e2add4601665c3fb08c8ea5b63fe73be4a3e2a66
frakyn <frantisek.kynych@tul.cz>
2018-04-24 21:23:45 +0200

    test4

--- dvojka.java
+++ dvojka.java
@@ -13,3 +13,51 @@
         return date.get();
     }
 
+/*
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty username;
+    private final SimpleStringProperty message;
+
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+    public String getDate() {
+        return date.get();
+    }
+
+
+/*
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty username;
+    private final SimpleStringProperty message;
+
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+    public String getDate() {
+        return date.get();
+    }
+
+
+/*
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty username;
+    private final SimpleStringProperty message;
+
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+    public String getDate() {
+        return date.get();
+    }
+
+s
