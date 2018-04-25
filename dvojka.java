0e7f597ecf5a5e49667019fa1c6598f0810dcc49
frakyn <frantisek.kynych@tul.cz>
2018-04-25 18:26:16 +0200

    test7

--- dvojka.java
+++ dvojka.java
@@ -8,18 +8,6 @@
         this.username = new SimpleStringProperty(username);
         this.message = new SimpleStringProperty(filename);
     }
-e;
-
-    public TableCommit(String date, String username, String filename) {
-        this.date = new SimpleStringProperty(date);
-        this.username = new SimpleStringProperty(username);
-        this.message = new SimpleStringProperty(filename);
-    }
-
-    public String getDate() {
-        return date.get();
-    }
-
 
 /*
     private final SimpleStringProperty date;
