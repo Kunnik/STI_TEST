bd6df5969089ed98acbf1bd92ca0025b1d8d6aa6
frakyn <frantisek.kynych@tul.cz>
2018-04-25 20:02:10 +0200

    test9

--- dvojka.java
+++ dvojka.java
@@ -15,6 +15,36 @@
     private final SimpleStringProperty message;
 
     public TableCommit(String date, String username, String filename) {
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+/*
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty username;
+    private final SimpleStringProperty message;
+
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+/*
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty username;
+    private final SimpleStringProperty message;
+
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
         this.date = new SimpleStringProperty(date);
         this.username = new SimpleStringProperty(username);
         this.message = new SimpleStringProperty(filename);
