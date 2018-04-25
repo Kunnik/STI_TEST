a01ad3c814277141772a651dbef66be702326f0d
frakyn <frantisek.kynych@tul.cz>
2018-04-25 18:27:19 +0200

    test8

--- dvojka.java
+++ dvojka.java
@@ -23,5 +23,68 @@
     public String getDate() {
         return date.get();
     }
+ame;
+    private final SimpleStringProperty message;
 
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+/*
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty username;
+    private final SimpleStringProperty message;
+
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+    public String getDate() {
+        return date.get();ame;
+    private final SimpleStringProperty message;
+
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+/*
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty username;
+    private final SimpleStringProperty message;
+
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+    public String getDate() {
+        return date.get();ame;
+    private final SimpleStringProperty message;
+
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+/*
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty username;
+    private final SimpleStringProperty message;
+
+    public TableCommit(String date, String username, String filename) {
+        this.date = new SimpleStringProperty(date);
+        this.username = new SimpleStringProperty(username);
+        this.message = new SimpleStringProperty(filename);
+    }
+
+    public String getDate() {
+        return date.get();
 s
